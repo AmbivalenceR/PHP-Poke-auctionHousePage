29aaaa4400c60c66999cf9984d422255d94666a7
AmbivalenceR <ambivalence2806@gmail.com>
2022-06-16 09:35:00 +0200

    Annonces, bdd, profil

--- src/profil.php
+++ src/profil.php
@@ -3,13 +3,16 @@
 include __DIR__ . "/accueil.php";
 include __DIR__ . "/includes/request.annonce.include.php";
 
+
 ?>
 
+
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
 <head>
     <meta charset="utf-8" />
+
     <title>Mes annonces</title>
     <!--FONTS-->
 
@@ -20,7 +23,23 @@ include __DIR__ . "/includes/request.annonce.include.php";
 <body>
     <header></header>
     <main>
-        <?= $annonceCree->afficherAnnonce(); ?>
+        <?php foreach ($annonces as $index => $annonce) { ?>
+
+            <h2 class="nomPokemon"><?= $annonce["nom"] ?></h2>
+            <p> Type : <?= $annonce["type"] ?> </p>
+            <p> PV : <?= $annonce["pv"] ?> </p>
+            <p> Description du Pokemon : <?= $annonce["description"] ?></p>
+            <p> Rareté de la carte : <?= $annonce["rarete"] ?></p>
+            <p> Numero de série de la carte : <?= $annonce["n_serie"] ?></p>
+            <p> État de la carte : <?= $annonce["condition"] ?></p>
+            <p> Prix de départ de enchères : <?= $annonce["prix_depart"] ?></p>
+            <p> Date : <?= $annonce["date_annonce"] ?></p>
+            <p> Date de fin des enchères : <?= $annonce["date_de_fin"] ?></p>
+            <p> Dernière enchère : X</p>
+            <p> ID annonce : <?= $annonce["id"] ?></p>
+            <p> ID utilisateur : <?= $annonce["id_utilisateur"] ?></p>
+
+        <?php } ?>
     </main>
     <footer></footer>
 </body>
